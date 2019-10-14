97cfc03e8f0858a53f3f8d4c51e4daae9254a35c
追梦小窝 <mail@54zm.com>
2019-10-14 19:45:46 +0800

    继续开始博客后台制作

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -42,8 +42,7 @@
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
+        <span> 请您</span>
       </div>
 
     </el-form>
